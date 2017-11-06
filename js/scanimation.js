87b40ddcb08526cac6d416132a10930713447d15
Shun Miyaki <shunmiyaki@hotmail.com>
2017-11-07 02:11:45 +0900

    scanimation done

--- js/scanimation.js
+++ js/scanimation.js
@@ -3,22 +3,35 @@ $(document).ready(function () {
 // INITIALISE HEADER END
 
 // MAIN FUNCTION BLOCK START
-  var target = $('#content');
+
+    $(window).keyup(function(e) {
+      console.log(e.which);
+      if (e.which == 48) {
+          $('#content').addClass('animate');
+        return false;
+      }
+    });
+
+  /*
+  //This worked as well but the scroll might be dependant on the user settings.
   $(window).scroll(function () {
-    var documentHeight = $(document).height(),
+    var target = $('#content'),
+        documentHeight = $(document).height(),
         distanceScrolled = $(this).scrollTop(),
         windowHeight = $(this).height(),
         windowWidth = $(this).width(),
         targetWidth = (target.width() - windowWidth),
         scrollPercent = (distanceScrolled / (documentHeight - windowHeight));
     target.css({
-        'left': ((targetWidth * scrollPercent) * -1)
+        'left': (((targetWidth * scrollPercent) * -1) + $(document).width())
     });
+
     // This worked as well, but it wasn't smooth. Probably a heavy function?
     // target.stop().animate({
     //     left: ((targetWidth * scrollPercent) * -1) + 'px'
     // });
   });
+  */
 // MAIN FUNCTION BLOCK END
 
 // INITIALISE FOOTER START
